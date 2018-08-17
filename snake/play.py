c029f8161c8b61a3a640d452ff11d601d9524a95
akhilc47 <akhilc1990@gmail.com>
2018-08-17 19:48:29 +0530

    score screen

--- snake/play.py
+++ snake/play.py
@@ -9,9 +9,9 @@ def main():
 
     # Display parameters
     cell_size = 20  # Size of the smallest occupiable block
-    board_size = 400  # Size of entire board NxN
+    board_size = 600  # Size of entire board NxN
     speed = 20  # Snake will move this much pixels every frame.
-    font = pygame.font.SysFont('Comic Sans MS', 20)
+    font = pygame.font.SysFont('Comic Sans MS', int(board_size*0.08))
     direction_dict = {pygame.K_LEFT: [-1, 0],
                       pygame.K_RIGHT: [1, 0],
                       pygame.K_DOWN: [0, 1],
@@ -27,7 +27,7 @@ def main():
                 sys.exit()
 
             elif game_over:
-                if event.type == pygame.KEYDOWN:
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                     snake = SnakeBoard(cell_size, board_size, speed)
                     game_over = False
                 else:
@@ -42,6 +42,8 @@ def main():
         if snake.move_snake():
             label = font.render("Game Over, Score: %s" % (len(snake.cells)-1), 1, (255, 255, 0))
             snake.game_window.blit(label, label.get_rect(center=(board_size//2, board_size//2)))
+            label = font.render("Press SPACE or Close", 1, (255, 255, 0))
+            snake.game_window.blit(label, label.get_rect(center=(board_size // 2, 3*board_size // 4)))
             pygame.display.update()
             game_over = True
 
