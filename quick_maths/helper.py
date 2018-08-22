596a3b7f5f417127d6ab539586c39a6a7389c698
akhilc47 <akhilc1990@gmail.com>
2018-08-23 01:48:45 +0530

    bot works on separate channels/servers independently

--- quick_maths/helper.py
+++ quick_maths/helper.py
@@ -1,28 +1,52 @@
+import configparser
 import random
+from collections import defaultdict
 
 
-def get_puzzle():
-    operator = random.choice(['+', '+', '-', '*', '*', '/']) # Twice as likely to get addition or multiplication
+class QuickMathGame:
+    def __init__(self):
+        self.answer = 0
+        self.game_running = False
+        self.players = defaultdict(lambda: 0, {})
+        self.question, self.answer = '', 0
+        self.get_puzzle()
 
-    if operator == '+':
-        arg1 = random.randint(0, 99)
-        arg2 = random.randint(0, 99)
-        answer = arg1+arg2
+    def get_puzzle(self):
+        """
+        Creates a math question which has a single, basic operation.
+        :return: returns a string (in the format <arg1> <operator> <arg2>) and an int which is the answer
+        """
+        operator = random.choice(['+', '+', '-', '*', '*', '/'])  # Twice as likely to get addition or multiplication
 
-    elif operator == '-':
-        arg1 = random.randint(0, 99)
-        arg2 = random.randint(0, arg1)
-        answer = arg1-arg2
+        if operator == '+':
+            arg1 = random.randint(0, 99)
+            arg2 = random.randint(0, 99)
+            answer = arg1+arg2
 
-    elif operator == '*':
-        arg1 = random.randint(0, 50)
-        arg2 = random.randint(0, 10)
-        answer = arg1*arg2
+        elif operator == '-':
+            arg1 = random.randint(0, 99)
+            arg2 = random.randint(0, arg1)
+            answer = arg1-arg2
 
-    else:
-        arg1 = random.randint(0, 20)
-        arg2 = random.randint(1, arg1)
-        answer = arg1*arg2
-        arg1, answer = answer, arg1
+        elif operator == '*':
+            arg1 = random.randint(0, 50)
+            arg2 = random.randint(0, 10)
+            answer = arg1*arg2
 
-    return '%d %s %d' % (arg1, operator, arg2), answer
+        else:
+            arg1 = random.randint(0, 20)
+            arg2 = random.randint(1, arg1)
+            answer = arg1*arg2
+            arg1, answer = answer, arg1
+
+        self.question, self.answer = '%d %s %d' % (arg1, operator, arg2), answer
+
+
+def get_token():
+    """
+    Reads the token stored in "config.ini"
+    :return:
+    """
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['REMOTE']['token']
--- quick_maths/script.py
+++ quick_maths/script.py
@@ -1,18 +1,13 @@
+import asyncio
 import discord
-import time
 import operator
-from collections import defaultdict
 from discord.ext.commands import Bot
-from helper import get_puzzle
+from helper import QuickMathGame, get_token
 
 COMMAND_PREFIX = 'qm '
-TOKEN = 'token goes here'
+TOKEN = get_token()
 client = Bot(command_prefix=COMMAND_PREFIX)
-
-answer = 0
-game_running = False
-players = {}
-players = defaultdict(lambda : 0, players)
+game = {}
 
 
 @client.event
@@ -20,41 +15,46 @@ async def on_ready():
     print('started!')
 
 
-@client.command(name='start', description='type "qm start" to start the game')
-async def start_puzzle():
-
-    global game_running, answer
-    if not game_running:
-        question, answer = get_puzzle()
-        print(question, answer)
-        await client.say(question)
-        game_running = True
+@client.command(name='start', description='type "qm start" to start the game', pass_context=True)
+async def start_puzzle(context):
+    global game
+    channel = context.message.channel
+    print(channel.name, channel.id)
+    if channel.id not in game:
+        game[channel.id] = QuickMathGame()
+    print(channel.name, channel.id)
+    if not game[channel.id].game_running:
+        print(game[channel.id].question, game[channel.id].answer)
+        await client.say(game[channel.id].question)
+        game[channel.id].game_running = True
     else:
         await client.say('Game already started')
 
 
-@client.command(name='stop', description='type "qm stop" to stop the game')
-async def stop_puzzle():
-    global game_running
-    game_running= False
+@client.command(name='stop', description='type "qm stop" to stop the game', pass_context=True)
+async def stop_puzzle(context):
+    global game
+    channel = context.message.channel
+    game[channel.id].game_running = False
     await client.say('Game has been stopped')
     return
 
 
-@client.command(name='score', description='type "qm score" to display scores')
-async def show_score():
-    global players
-    if not players:
+@client.command(name='score', description='type "qm score" to display scores', pass_context=True)
+async def show_score(context):
+    global game
+    channel = context.message.channel
+    if not game[channel.id].players:
         return
-    await client.say(embed=show_score_backend())
+    await client.say(embed=show_score_backend(channel.id))
     return
 
 
-def show_score_backend():
-    global players
-    if not players:
+def show_score_backend(channel_id):
+    global game
+    if not game[channel_id].players:
         return
-    sorted_scores = sorted(players.items(), key=operator.itemgetter(1), reverse=True)
+    sorted_scores = sorted(game[channel_id].players.items(), key=operator.itemgetter(1), reverse=True)
     embed = discord.Embed(title="Session Scoreboard", color=0x00ff00, inline=True)
     names, scores = '', ''
     for line in sorted_scores:
@@ -67,23 +67,25 @@ def show_score_backend():
 
 @client.event
 async def on_message(message):
-
-    global game_running, answer, players
+    global game
+    channel = message.channel
     if message.author == client.user:
         return
-    elif game_running:
+    elif channel.id in game.keys() and game[channel.id].game_running:
         try:
             int(message.content)
-            if message.content == str(answer):
-                players[message.author] += 10
-                await client.send_message(message.channel, '%s: %d is correct' % (message.author.mention, answer))
-                await client.send_message(message.channel, embed=show_score_backend())
-                time.sleep(3)
-                question, answer = get_puzzle()
-                print(question, answer)
-                await client.send_message(message.channel, question)
+            if message.content == str(game[channel.id].answer):
+                game[channel.id].players[message.author] += 10
+                await client.send_message(message.channel, '%s: %d is correct' %
+                                          (message.author.mention, game[channel.id].answer))
+                await client.send_message(message.channel, embed=show_score_backend(channel.id))
+                await asyncio.sleep(3)
+                game[channel.id].get_puzzle()
+                print(game[channel.id].question, game[channel.id].answer)
+                await client.send_message(message.channel, game[channel.id].question)
             else:
-                await client.send_message(message.channel, '%s: %s is wrong' % (message.author.mention, message.content))
+                await client.send_message(message.channel, '%s: %s is wrong' %
+                                          (message.author.mention, message.content))
         except ValueError:
             print('input was not a number')
     await client.process_commands(message)
